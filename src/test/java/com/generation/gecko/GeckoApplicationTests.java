abb9fef579974c190e25aef633fce382414062eb
Lais <lais.salesms@gmail.com>
2023-04-18 09:33:47 -0300

    Criando Projeto Spring

---
+++ src/test/java/com/generation/gecko/GeckoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gecko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeckoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
