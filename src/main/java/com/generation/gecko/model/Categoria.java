1f71c63840b92a467ef6133816a2819884d2c7fa
Kaique <kaique.ferr@gmail.com>
2023-04-25 10:32:09 -0300

    criado produtoRepository e editado relacionamentos

--- src/main/java/com/generation/gecko/model/Categoria.java
+++ src/main/java/com/generation/gecko/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.gecko.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@NotBlank(message = "O atributo 'descrição' é obrigatório.")
 	@Size(min = 5, max = 255, message = "A descrição da categoria deve ter entre 5 e 255 caracteres.")
 	private String descricao;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -47,6 +58,16 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 	
 	
 
---
+++ src/main/java/com/generation/gecko/repository/ProdutoRepository.java
@@ -0,0 +1,20 @@
+package com.generation.gecko.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.gecko.model.Categoria;
+import com.generation.gecko.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+	public List<Produto> findAllByNomeContainingIgnoreCaseOrDescricaoContainingIgnoreCase(@Param("nome") String nome, @Param("descricao") String descricao);   
+	
+	public List<Produto> findAllByReciclavelTrue();
+	
+	public List<Produto> findAllByEstadoTrue();
+	
+	public List<Produto> findAllByEstadoFalse();
+}
