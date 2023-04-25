6189fa75429395dfcf42e2c2a2d75402eee7822d
JULIAVALERIO <juliav286@gmail.com>
2023-04-25 09:54:47 -0300

    adicionando class model produto

---
+++ src/main/java/com/generation/gecko/model/Produto.java
@@ -0,0 +1,124 @@
+package com.generation.gecko.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name ="tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo 'nome' é obrigatório.")
+	@Size(min = 1, max = 255, message = "O nome da categoria deve ter entre 1 e 255 caracteres.")
+	private String nome;
+	
+	@NotBlank(message = "O atributo 'descrição' é obrigatório.")
+	@Size(min = 1, max = 255, message = "O nome da descrição deve ter entre 1 e 255 caracteres.")
+	private String descricao;
+	
+	@NotEmpty
+	@PositiveOrZero
+	private BigDecimal preco;
+	
+	@NotEmpty
+	private boolean estado;
+	
+	@NotEmpty
+	private boolean reciclavel;
+	
+	@NotEmpty
+	private String foto;
+	
+	private Integer curtidas;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public boolean isReciclavel() {
+		return reciclavel;
+	}
+
+	public void setReciclavel(boolean reciclavel) {
+		this.reciclavel = reciclavel;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Integer getCurtidas() {
+		return curtidas;
+	}
+
+	public void setCurtidas(Integer curtidas) {
+		this.curtidas = curtidas;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
